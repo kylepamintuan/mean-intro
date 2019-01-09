fe3dd0f93f92e1ed52d828e36677d8c395eac391
kylepamintuan <kylepamintuan@gmail.com>
2019-01-09 10:03:51 -0800

    moved the backend to its own repo and removed the modules & dependencies from this repo.

--- src/app/request-to-backend.service.ts
+++ src/app/request-to-backend.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { User } from './models/user';
 
@@ -27,14 +27,8 @@ export class RequestToBackendService {
 
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
+      console.error(error);
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      // this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
